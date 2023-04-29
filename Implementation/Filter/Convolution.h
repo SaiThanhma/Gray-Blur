d62aaa54c4618817fd1926fdec87b165738d6dc2
congthanhdang <ge42xag@mytum.de>
2023-04-29 11:34:24 +0200

    convolution templated structure

--- Implementation/Filter/Convolution.h
+++ Implementation/Filter/Convolution.h
@@ -1,37 +1,68 @@
 #include "Mymath.h"
+#include <cstdio>
+#include <cinttypes>
 #include <array>
 
-constexpr float hg(int i, int j, int meani, int meanj, int sigma){
 
-    return mymath::exp(-0.5 * ((mymath::pow(i - meani, 2) + mymath::pow(j - meanj, 2)) / mymath::pow(sigma, 2)));
-}
+namespace Convolution{
 
-template<unsigned M, unsigned N, unsigned sigma>
-constexpr std::array<float, M * N> gaussian_filter(){
+    template<unsigned M, unsigned N, unsigned sigma>
+    constexpr std::array<float, M * N> gaussian_filter();
 
-    std::array<float, M * N> kernel;
-    float sum = 0.0;
-    float tmp = 0.0;
-    int meani = M /2;
-    int meanj = N /2;
-    int pos = 0;
+    constexpr float hg(int i, int j, int meani, int meanj, int sigma);
 
-    for(int i = 0; i < M; ++i){
-        for(int j = 0; j < N; ++j, ++pos){
-            tmp = hg(i, j, meani, meanj, sigma);
-            sum += tmp; 
-            kernel.at(pos) = tmp;
+    template<unsigned kernelwidth, unsigned kernelheight, std::array<float, kernelwidth * kernelheight> kernel>
+    class Convolution {
+        void convolution(const uint8_t* img_in, size_t width, size_t height, uint8_t* img_out);  
+    };
+
+
+    template<unsigned M, unsigned N, unsigned sigma>
+    constexpr std::array<float, M * N> gaussian_filter(){
+
+        std::array<float, M * N> kernel;
+        float sum = 0.0;
+        float tmp = 0.0;
+        int meani = M /2;
+        int meanj = N /2;
+        int pos = 0;
+
+        for(int i = 0; i < M; ++i){
+            for(int j = 0; j < N; ++j, ++pos){
+                tmp = hg(i, j, meani, meanj, sigma);
+                sum += tmp; 
+                kernel.at(pos) = tmp;
+            }
         }
-    }
-    if(sum != 0.0){
-        for(int i = 0; i < kernel.size(); ++i){
-            kernel.at(i) /= sum;
+        if(sum != 0.0){
+            for(int i = 0; i < kernel.size(); ++i){
+                kernel.at(i) /= sum;
+            }
         }
+        
+        return kernel;
     }
-    
-    return kernel;
+
+    constexpr float hg(int i, int j, int meani, int meanj, int sigma){
+        return mymath::exp(-0.5 * ((mymath::pow(i - meani, 2) + mymath::pow(j - meanj, 2)) / mymath::pow(sigma, 2)));
+    }
+
+
+    template<unsigned kernelheight, std::array<float, kernelheight> kernel>
+    class Convolution<1, kernelheight, kernel> {
+        void convolution(const uint8_t* img_in, size_t width, size_t height, uint8_t* img_out){
+            
+        }
+    };
+
+    template<unsigned kernelwidth, std::array<float, kernelwidth> kernel>
+    class Convolution<kernelwidth, 1, kernel> {
+        void convolution(const uint8_t* img_in, size_t width, size_t height, uint8_t* img_out){
+
+        }
+    };
+
+
+
 }
-
-void convolution(const uint8_t* img_in, size_t width, size_t height, uint8_t* img_out, std::vector<float> kernel);
-
 
