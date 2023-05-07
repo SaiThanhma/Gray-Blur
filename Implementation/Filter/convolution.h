b2caa3bc9c71829e71d2b226f805304eef9be844
congthanhdang <ge42xag@mytum.de>
2023-05-07 16:02:19 +0200

    save

--- Implementation/Filter/convolution.h
+++ Implementation/Filter/convolution.h
@@ -5,26 +5,22 @@
 #include <memory>
 #include <cmath>
 
-template<size_t _Size>
-using Kernel = std::array<float,_Size>;
+template <size_t _Size>
+using Kernel = std::array<float, _Size>;
 
-template<size_t _Size>
+template <size_t _Size>
 using Coordinates = std::array<std::pair<int, int>, _Size>;
 
 template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
 void convolution(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, std::array<float, kernelheight * kernelwidth> kernel);
 
-template <typename T, unsigned kernelheight, unsigned kernelwidth>
-constexpr float kernelcalc(const T *buffer, std::array<float, kernelheight * kernelwidth> kernel);
-
-template <unsigned kernelheight, unsigned kernelwidth>
-constexpr void getIndicies(std::array<std::pair<int, int>, kernelheight * kernelwidth> &indicies,int i, int j, size_t width, size_t height);
-
 template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
-constexpr void channelhandler(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int pos, Kernel<kernelheight * kernelwidth> kernel, Coordinates<kernelheight * kernelwidth> &indicies, T *buffer);
+constexpr void kernelCalculation(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int i, int j, Kernel<kernelheight * kernelwidth> kernel, Coordinates<kernelheight * kernelwidth> &indicies, T *buffer);
+
+template <typename T, unsigned kernelheight, unsigned kernelwidth>
+constexpr float weightedSum(const T *buffer, std::array<float, kernelheight * kernelwidth> kernel);
 
 constexpr std::pair<int, int> mirrorindex(int posx, int posy, int endx, int endy);
-
 
 template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
 void convolution(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, std::array<float, kernelheight * kernelwidth> kernel)
@@ -32,21 +28,18 @@ void convolution(const T *img_in, size_t width, size_t height, size_t channels, 
     std::reverse(kernel.begin(), kernel.end());
     std::unique_ptr<T[]> buffer = std::make_unique<T[]>(kernelheight * kernelwidth);
     Coordinates<kernelheight * kernelwidth> indicies;
-    int kernelwidthradius = (kernelwidth - 1) / 2;
-    int kernelheightradius = (kernelheight - 1) / 2;
 
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width; ++j)
         {
-            getIndicies<kernelheight, kernelwidth>(indicies, i, j , width, height);
-            channelhandler<T, kernelheight, kernelwidth, gray>(img_in, width, height, channels, img_out, (i * width + j) * channels, kernel, indicies, buffer.get());    
+            kernelCalculation<T, kernelheight, kernelwidth, gray>(img_in, width, height, channels, img_out, i, j, kernel, indicies, buffer.get());
         }
     }
 }
 
 template <typename T, unsigned kernelheight, unsigned kernelwidth>
-constexpr float kernelcalc(const T *buffer, Kernel<kernelheight * kernelwidth> kernel)
+constexpr float weightedSum(const T *buffer, Kernel<kernelheight * kernelwidth> kernel)
 {
     float sum = 0;
     for (int i = 0; i < kernel.size(); ++i)
@@ -57,38 +50,44 @@ constexpr float kernelcalc(const T *buffer, Kernel<kernelheight * kernelwidth> k
     return sum;
 }
 
-template <unsigned kernelheight, unsigned kernelwidth>
-constexpr void getIndicies(Coordinates<kernelheight * kernelwidth> &indicies,int i, int j, size_t width, size_t height){
-    int pos = 0;
+template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
+constexpr void kernelCalculation(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int i, int j, Kernel<kernelheight * kernelwidth> kernel, Coordinates<kernelheight * kernelwidth> &indicies, T *buffer)
+{
+    int bufferindex = 0;
     int starty = -static_cast<int>((kernelheight + 1) / 2 - 1);
     int endy = (kernelheight + 1) / 2 - 1;
 
     int startx = -static_cast<int>((kernelwidth + 1) / 2 - 1);
     int endx = (kernelwidth + 1) / 2 - 1;
 
-    for (int k = starty; k <= endy; ++k)
+    int pos = (i * width + j) * channels;
+
+    if constexpr (gray)
     {
-        for (int l = startx; l <= endx; ++l, ++pos)
-        {
-            indicies.at(pos) = mirrorindex(i + k, j + l, height - 1, width - 1);
-        }
-    }
-}
 
-template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
-constexpr void channelhandler(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int pos, Kernel<kernelheight * kernelwidth> kernel, Coordinates<kernelheight * kernelwidth> &indicies, T *buffer){
-    if constexpr(gray){
-        
-        for (int n = 0; n < indicies.size(); ++n)
+        for (int k = starty; k <= endy; ++k)
         {
-            buffer[n] = img_in[(indicies.at(n).first * width + indicies.at(n).second) * channels];
+            for (int l = startx; l <= endx; ++l, ++bufferindex)
+            {
+                auto index = mirrorindex(i + k, j + l, height - 1, width - 1);
+                buffer[bufferindex] = img_in[(index.first * width + index.second) * channels];
+            }
         }
 
-        img_out[pos] = static_cast<T>(std::ceil(kernelcalc<T, kernelheight, kernelwidth>(buffer, kernel)));
+        img_out[pos] = static_cast<T>(std::ceil(weightedSum<T, kernelheight, kernelwidth>(buffer, kernel)));
         img_out[pos + 1] = img_out[pos];
         img_out[pos + 2] = img_out[pos];
     }
-    else{
+    else
+    {
+        for (int k = starty; k <= endy; ++k)
+        {
+            for (int l = startx; l <= endx; ++l, ++bufferindex)
+            {
+                indicies.at(bufferindex) = mirrorindex(i + k, j + l, height - 1, width - 1);
+            }
+        }
+
         for (int m = 0; m < channels; ++m)
         {
             for (int n = 0; n < indicies.size(); ++n)
@@ -96,7 +95,7 @@ constexpr void channelhandler(const T *img_in, size_t width, size_t height, size
                 buffer[n] = img_in[(indicies.at(n).first * width + indicies.at(n).second) * channels + m];
             }
 
-            img_out[pos + m] = static_cast<T>(std::ceil(kernelcalc<T, kernelheight, kernelwidth>(buffer, kernel)));
+            img_out[pos + m] = static_cast<T>(std::ceil(weightedSum<T, kernelheight, kernelwidth>(buffer, kernel)));
         }
     }
 }
