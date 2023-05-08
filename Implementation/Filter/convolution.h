ec8ea3cb6bc671c6c50b4668cffbed6ca45acd7e
congthanhdang <ge42xag@mytum.de>
2023-05-08 22:58:15 +0200

    convolution rework

--- Implementation/Filter/convolution.h
+++ Implementation/Filter/convolution.h
@@ -4,118 +4,114 @@
 #include <tuple>
 #include <memory>
 #include <cmath>
+#include <vector>
+#include <iostream>
 
-template <size_t _Size>
-using Kernel = std::array<float, _Size>;
+using Kernel = std::vector<std::vector<float>>;
+using Coordinates = std::vector<std::pair<int, int>>;
 
-template <size_t _Size>
-using Coordinates = std::array<std::pair<int, int>, _Size>;
-
-template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
-void convolution(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, std::array<float, kernelheight * kernelwidth> kernel);
-
-template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
-constexpr void kernelCalculation(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int i, int j, Kernel<kernelheight * kernelwidth> kernel, Coordinates<kernelheight * kernelwidth> &indicies, T *buffer);
-
-template <typename T, unsigned kernelheight, unsigned kernelwidth>
-constexpr float weightedSum(const T *buffer, std::array<float, kernelheight * kernelwidth> kernel);
-
-constexpr std::pair<int, int> mirrorindex(int posx, int posy, int endx, int endy);
-
-template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
-void convolution(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, std::array<float, kernelheight * kernelwidth> kernel)
+struct Index
 {
-    std::reverse(kernel.begin(), kernel.end());
-    std::unique_ptr<T[]> buffer = std::make_unique<T[]>(kernelheight * kernelwidth);
-    Coordinates<kernelheight * kernelwidth> indicies;
+    static constexpr auto mirrorindex = [](int height, int width, int kernelheight, int kernelwidth)
+    {
+        int endy = height - 1;
+        int endx = width - 1;
+
+        int kernelwidthradius = (kernelwidth - 1) / 2;
+        int kernelheightradius = (kernelheight - 1) / 2;
+
+        int nheight = height + kernelheightradius * 2;
+        int nwidth = width + kernelwidthradius * 2;
+
+        int size = (2 * kernelwidthradius + width) * (2 * kernelheightradius + height);
+        Coordinates c;
+        c.reserve(size);
+
+        int i = 0;
+        for (int posy = -kernelheightradius; posy < nheight - kernelheightradius; ++posy)
+        {
+            for (int posx = -kernelwidthradius; posx < nwidth - kernelwidthradius; ++posx, ++i)
+            {
+                if ((posx >= 0 && posx <= endx) && (posy >= 0 && posy <= endy))
+                {
+                    c.emplace_back(posy, posx);
+                    continue;
+                }
+
+                int distancey = posy < 0 ? -posy : posy;
+                int repy = distancey / endy;
+                int mody = distancey % endy;
+                int indexy = repy % 2 == 0 ? mody : endy - mody;
+
+                int distancex = posx < 0 ? -posx : posx;
+                int repx = distancex / endx;
+                int modx = distancex % endx;
+                int indexx = repx % 2 == 0 ? modx : endx - modx;
+
+                c.emplace_back(indexy, indexx);
+            }
+        }
+
+        return c;
+    };
+};
+
+template <typename T>
+constexpr void convolution(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, const Kernel &kernel);
+
+template <typename T>
+constexpr void kernelCalculation(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int i, int j, const Kernel &kernel, Coordinates &coordinates, int shift);
+
+template <typename T>
+constexpr void convolution(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, const Kernel &kernel)
+{
+    int kernelheight = kernel.size();
+    int kernelwidth = kernel.at(0).size();
+
+    int kernelwidthradius = (kernelwidth - 1) / 2;
+    int kernelheightradius = (kernelheight - 1) / 2;
+    int nwidth = width + kernelwidthradius * 2;
+    int shift = nwidth * kernelheightradius + kernelwidthradius;
+
+    Coordinates c = Index::mirrorindex(height, width, kernelheight, kernelwidth);
 
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width; ++j)
         {
-            kernelCalculation<T, kernelheight, kernelwidth, gray>(img_in, width, height, channels, img_out, i, j, kernel, indicies, buffer.get());
+            kernelCalculation<T>(img_in, width, height, channels, img_out, i, j, kernel, c, shift);
         }
     }
 }
 
-template <typename T, unsigned kernelheight, unsigned kernelwidth>
-constexpr float weightedSum(const T *buffer, Kernel<kernelheight * kernelwidth> kernel)
+template <typename T>
+constexpr void kernelCalculation(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int i, int j, const Kernel &kernel, Coordinates &coordinates, int shift)
 {
-    float sum = 0;
-    for (int i = 0; i < kernel.size(); ++i)
+
+    int kernelwidth = kernel.at(0).size();
+    int kernelheight = kernel.size();
+
+    int kernelwidthradius = (kernelwidth - 1) / 2;
+    int kernelheightradius = (kernelheight - 1) / 2;
+    int nwidth = width + kernelwidthradius * 2;
+
+    for (int k = 0; k < channels; ++k)
     {
-        sum += static_cast<float>(buffer[i]) * kernel.at(i);
-    }
+        float sum = 0;
+        int pos = (i * nwidth + j);
+        int shiftkernel = pos - kernelwidthradius + nwidth * kernelheightradius;
 
-    return sum;
-}
-
-template <typename T, unsigned kernelheight, unsigned kernelwidth, bool gray>
-constexpr void kernelCalculation(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, int i, int j, Kernel<kernelheight * kernelwidth> kernel, Coordinates<kernelheight * kernelwidth> &indicies, T *buffer)
-{
-    int bufferindex = 0;
-    int starty = -static_cast<int>((kernelheight + 1) / 2 - 1);
-    int endy = (kernelheight + 1) / 2 - 1;
-
-    int startx = -static_cast<int>((kernelwidth + 1) / 2 - 1);
-    int endx = (kernelwidth + 1) / 2 - 1;
-
-    int pos = (i * width + j) * channels;
-
-    if constexpr (gray)
-    {
-
-        for (int k = starty; k <= endy; ++k)
+        for (int l = 0; l < kernel.size(); ++l, shiftkernel -= nwidth)
         {
-            for (int l = startx; l <= endx; ++l, ++bufferindex)
+            for (int m = 0; m < kernel.at(0).size(); ++m)
             {
-                auto index = mirrorindex(i + k, j + l, height - 1, width - 1);
-                buffer[bufferindex] = img_in[(index.first * width + index.second) * channels];
+                int y = coordinates.at(shift + shiftkernel + m).first;
+                int x = coordinates.at(shift + shiftkernel + m).second;
+
+                sum += img_in[y * width * channels + x * channels + k] * kernel.at(l).at(m);
             }
         }
 
-        img_out[pos] = static_cast<T>(weightedSum<T, kernelheight, kernelwidth>(buffer, kernel));
-        img_out[pos + 1] = img_out[pos];
-        img_out[pos + 2] = img_out[pos];
+        img_out[(i * width + j) * channels + k] = static_cast<T>(sum);
     }
-    else
-    {
-        for (int k = starty; k <= endy; ++k)
-        {
-            for (int l = startx; l <= endx; ++l, ++bufferindex)
-            {
-                indicies.at(bufferindex) = mirrorindex(i + k, j + l, height - 1, width - 1);
-            }
-        }
-
-        for (int m = 0; m < channels; ++m)
-        {
-            for (int n = 0; n < indicies.size(); ++n)
-            {
-                buffer[n] = img_in[(indicies.at(n).first * width + indicies.at(n).second) * channels + m];
-            }
-
-            img_out[pos + m] = static_cast<T>(weightedSum<T, kernelheight, kernelwidth>(buffer, kernel));
-        }
-    }
-}
-
-constexpr std::pair<int, int> mirrorindex(int posy, int posx, int endy, int endx)
-{
-    if ((posx >= 0 && posx <= endx) && (posy >= 0 && posy <= endy))
-    {
-        return std::make_pair(posy, posx);
-    }
-
-    int distancey = posy < 0 ? -posy : posy;
-    int repy = distancey / endy;
-    int mody = distancey % endy;
-    int indey = repy % 2 == 0 ? mody : endy - mody;
-
-    int distancex = posx < 0 ? -posx : posx;
-    int repx = distancex / endx;
-    int modx = distancex % endx;
-    int index = repx % 2 == 0 ? modx : endx - modx;
-
-    return std::make_pair(indey, index);
 }
