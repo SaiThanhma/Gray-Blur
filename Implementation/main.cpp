4b2860cae4512af2970dbc5aaba5636feb42ff7a
congthanhdang <ge42xag@mytum.de>
2023-05-12 17:17:10 +0200

    file io

--- Implementation/main.cpp
+++ Implementation/main.cpp
@@ -1,99 +1,161 @@
 #include "sobel.h"
 #include "gaussian.h"
 #include "convolution.h"
+#include "io.h"
 #include "Image/image.h"
+#include "bitmap.h"
 #include <iostream>
 #include <string>
 #include <getopt.h>
+#include <iomanip>
+#include <chrono>
 
-static void usage(char *argv0){
-    std::cout 
-    << "Usage: " << argv0 << " input output [-gray] [-gaussian k] [-sobel -g]" 
-    << "\n"
-    << "input: input file with [.jpg | .png | .bmp | .tga] as extension"
-    << "\n"
-    << "output: output file with [.jpg | .png | .bmp | .tga] as extension, otherwise output file will be png"
-    << "\n"
-    << "gray: grayscale filter with luminance as default"
-    << "\n"
-    << "gaussian: gaussian filter with kernel size k as parameter"
-    << "\n"
-    << "sobel: sobel operator with a 3x3 kernel and g flag as optional flag for additional color gradient"    
-    << std::endl;
+
+static void usage(char *argv0)
+{
+    std::cout
+        << "Usage: " << argv0 << " input output [-gray [\"r g b\"]] [-gaussian [\"seperate kSize border\"]] [-sobel [\"kSize gausskSize threshold gradient\"]]"
+        << "\n"
+        << "input: input file .bmp as extension."
+        << "\n"
+        << "output: output file with .bmp as extension, otherwise a .bmp file will be created."
+        << "\n"
+        << "gray: grayscale filter with \"r g b\" as weighted parameters. 0 <= r, b, g <= 1 with r + g + b = 1. Default is r = 0.3, g = 0.59, b = 0.11 (luma grayscaling)"
+        << "\n"
+        << "gaussian: gaussian filter with \"v k b\" If seperate = 0 then the image will be processed once with a kSize xkSize Kernel in quadratic time, "
+        << "otherwise image will be processed twice with a 1 x kSize and kSize x 1 Kernel in linear time. kSize has to be positive and odd. "
+        << "border can be chosen between 0 | 1 | 2 | 3, 0 = WO, 1 = EXTEND 2 = MIRROR, 3 = WRAP. "
+        << "Default is seperate = 1, k = 3, border = EXTEND"
+        << "\n"
+        << "sobel: sobel operator with a kSize x kSize Kernel, with kSize >= 3 and odd. The image will be preprocessed with a gausskSize x gausskSize gaussian Filter. "
+        << "The threshold is a number for an edge to be recognized between 0 <= threshold <= 255 (ideally about 45). If gradient = 1 then the gradient (orientation) will be also shown in colors "
+        << "Default is kSize = 5, gausskSize = 3, threshold = 45, gradient = 0"
+        << std::endl;
 }
 
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
-/*
-    std::unique_ptr<Bitmap> bmp = readFile("../Pictures/scrambled_egg.bmp");
-
-    copyBitmapFile("../Pictures/scrambled_egg.bmp", "../Pictures/scrambled_egg_blur.bmp");
     
-    std::unique_ptr<Bitmap> convbmp = readFile("../Pictures/scrambled_egg.bmp");
-    std::unique_ptr<uint8_t[]> tmp = std::make_unique<uint8_t[]>(bmp->biWidth * bmp->biHeight * 3);
-
-    convolution<1,25>(bmp->pixeldata.get(), bmp->biWidth, bmp->biHeight, tmp.get(), gaussian_filter<1,25, 10>());
-    convolution<25,1>(tmp.get(), bmp->biWidth, bmp->biHeight, convbmp->pixeldata.get(), gaussian_filter<25,1, 10>());
-    writeFile("../Pictures/scrambled_egg_blur.bmp", convbmp->pixeldata.get(), convbmp->biHeight * convbmp->biWidth * 3, bmp->bfOffBits, SEEK_SET);
-
-    
-
-   Image<uint8_t> image{"../Pictures/scrambled_egg.bmp"};
-   uint8_t *out = new uint8_t[image.width * image.height * 3];
-   convolution<uint8_t, 9, 9>(image.data.get(), image.width, image.height, 3, out, gaussian_filter<9,9,5>());
-
-   stbi_write_bmp("../Pictures/scrambled_eggtest.bmp",  image.width, image.height, 3, out);
-*/
-/*
-    Image<uint8_t> image{"../Pictures/lena.png"};
-    uint8_t *out = new uint8_t[image.width * image.height * image.channels];
-
-    //gray(image.data.get(), image.width, image.height, image.channels, out);
-    
-    sobel<uint8_t>(image.data.get(), image.width, image.height, image.channels, out);
-    //stbi_write_jpg("../Pictures/Fruits_sobel.jpeg",  image.width, image.height, image.channels, out, 100);
-    
-    
-
-    stbi_write_png("../Pictures/lena_sobelgradient.png",  image.width, image.height, image.channels, out, image.channels * image.width);
-        while( (c = getopt_long(argc, argv, ,&longindex)) != EOF )
+    if (argc < 2)
     {
-
+        usage(argv[0]);
+        return 1;
     }
-    */
-   if(argc < 3){
-       usage(argv[0]);
-       return 1;
-   }
 
     static struct option long_options[] = {
-        {"gray",            no_argument,          0,  'g' },
-        {"gaussianblur",    optional_argument,    0,  'b' },
-        {"sobel",           optional_argument,    0,  's' },
-        {0,                 0,                    0,   0  }
-    };
-    
+        {"gray", optional_argument, 0, 'g'},
+        {"gaussian", optional_argument, 0, 'b'},
+        {"sobel", optional_argument, 0, 's'},
+        {0, 0, 0, 0}};
+
+    IOhandler iohandler;
     int longindex;
     int opt;
-    while((opt = getopt_long(argc, argv, "gbs", long_options, &longindex)) != EOF){
-         switch (opt)
-         {
-         case 'g':
-             break;
-         case 'b':
+
+    bool grayf = false;
+    bool blurf = false;
+    bool sobelf = false;
+
+    while ((opt = getopt_long(argc, argv, "g::b::s::", long_options, &longindex)) != EOF)
+    {
+        switch (opt)
+        {
+        case 'g':
+        { 
+            if (optarg && !iohandler.grayHandler(optarg))
+            {
+                usage(argv[0]);
+                return 1;
+            }
+            grayf = true;
             break;
-         case 's':
+        }
+        case 'b':
+        {
+            if (optarg && !iohandler.gaussianHandler(optarg))
+            {
+                usage(argv[0]);
+                return 1;
+            }
+            blurf = true;
             break;
-         default:
-             usage(argv[0]);
-             return 1;
-         }
+        }
+        case 's':
+        {
+            if (optarg && !iohandler.sobelHandler(optarg))
+            {
+                usage(argv[0]);
+                return 1;
+            }
+            sobelf = true;
+            break;
+        }
+        default:
+        {
+            usage(argv[0]);
+            return 1;
+        }
+        }
     }
 
-    Image<uint8_t> img{argv[1]};
-    std::unique_ptr<uint8_t[]> odata = std::make_unique<uint8_t[]>(img.height * img.width * img.channels);
-    sobel<uint8_t>(img.data.get(), img.width, img.height, img.channels, odata.get());
-    Image<uint8_t>::write(argv[2], img.width, img.height, img.channels, odata.get()); 
+    if (optind >= argc || argc - optind != 2)
+    {
+        usage(argv[0]);
+        return 1;
+    }
+    std::string_view input{argv[optind++]};
+    std::string_view output{argv[optind]};
+
+    if (!iohandler.pathHandler(input.data()) || !iohandler.pathHandler(output.data()))
+    {
+        usage(argv[0]);
+        return 1;
+    }
+    
+    std::unique_ptr<Bitmap> img_in;
+    if(!(img_in = readBmpFile(input.data()))){
+        return 1;
+    }
+    if(!copyBitmapHeader(input.data(), output.data())){
+        return 1;
+    }
+    if(img_in->biHeight == 0 || img_in->biWidth == 0){
+        std::cout << "Picture height or width is 0" << std::endl;
+        return 1;
+    }
+
+    std::unique_ptr<uint8_t[]> odata = std::make_unique<uint8_t[]>(img_in->biHeight * img_in->biWidth * 3);
+    if(grayf){
+        auto startgray = std::chrono::system_clock::now(); 
+
+        auto endgray = std::chrono::system_clock::now();
+
+        float resgray = std::chrono::duration_cast<std::chrono::seconds>(endgray-startgray).count();
+
+        std::cout << "Time for gray: " << std::setprecision(5) << resgray << std::endl;
+
+    }
+    if(blurf)
+    {
+        auto startblur = std::chrono::system_clock::now(); 
+
+        auto endblur = std::chrono::system_clock::now();
+
+        float resblur = std::chrono::duration_cast<std::chrono::seconds>(endblur-startblur).count();
+        std::cout << "Time for blur: " << std::fixed <<std::setprecision(5) << resblur << std::endl;
+    }
+    if(sobelf){
+
+        auto startsobel = std::chrono::system_clock::now(); 
+
+        sobel<uint8_t>(img_in->data.get(), img_in->biWidth, img_in->biHeight, 3, odata.get());
+
+        auto endsobel = std::chrono::system_clock::now();
+        
+        float ressobel = std::chrono::duration_cast<std::chrono::seconds>(endsobel-startsobel).count();
+
+        std::cout << "Time for sobel: " << std::fixed << std::setprecision(2) << ressobel << std::endl;
+    }
 
     return 0;
 }
