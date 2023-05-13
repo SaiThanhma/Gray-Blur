0aca1414ab175639ed92601e3971f502b6b8f3a1
congthanhdang <ge42xag@mytum.de>
2023-05-13 13:36:10 +0200

    sobel code fatorized in magnitude and gradient

--- include/sobel.h
+++ include/sobel.h
@@ -11,10 +11,16 @@
 using Color = std::tuple<uint8_t, uint8_t, uint8_t>;
 
 template <typename T>
-void sobel(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, size_t ksize = 3, size_t gaussianKsize = 3, uint8_t threshold = 0, bool gradient = false);
+void sobel(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, size_t ksize = 3, size_t gaussianKsize = 3, size_t threshold = 0, bool showgradient = false);
 
 template <typename T>
 void getGyGx(const T *img_in, size_t width, size_t height, size_t channels, float *img_outY, float *img_outX, size_t ksize, size_t gaussianKsize);
+
+template <typename T>
+void magnitude(const float *gy, const float *gx, size_t width, size_t height, size_t channels, T *img_out, size_t threshold);
+
+template <typename T>
+void gradient(const float *gy, const float *gx, size_t width, size_t height, size_t channels, T *img_out, size_t threshold);
 
 template <typename T>
 void copy(const T *img_in, float *img_inX, float *img_inY, size_t size);
@@ -24,12 +30,12 @@ constexpr std::pair<Kernel, Kernel> sobelKernel(size_t ksize);
 constexpr void normalizeKernel(Kernel &first, Kernel &second);
 
 template <typename T>
-void rescaleLightDark(const float *img_in, size_t width, size_t height, size_t channels, T *img_out, uint8_t threshold);
+void rescaleLightDark(const float *img_in, size_t width, size_t height, size_t channels, T *img_out);
 
 Color colorGradient(double angle);
 
 template <typename T>
-void sobel(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, size_t ksize, size_t gaussianKsize, uint8_t threshold, bool gradient)
+void sobel(const T *img_in, size_t width, size_t height, size_t channels, T *img_out, size_t ksize, size_t gaussianKsize, size_t threshold, bool showgradient)
 {
     size_t size = width * height * channels;
 
@@ -37,58 +43,32 @@ void sobel(const T *img_in, size_t width, size_t height, size_t channels, T *img
     std::unique_ptr<float[]> gx = std::make_unique<float[]>(size);
 
     getGyGx(img_in, width, height, channels, gy.get(), gx.get(), ksize, gaussianKsize);
-    
-    if (gradient)
+
+    if (showgradient)
     {
-        for (size_t i = 0; i < size - channels + 1; i += channels)
-        {
-
-            //   img_out[i] = static_cast<uint8_t>(std::sqrt(std::pow((img_outX.get()[i]), 2) + std::sqrt(std::pow(img_outY.get()[i], 2))));
-            img_out[i] = static_cast<T>((std::abs(gy.get()[i]) + std::abs(gx.get()[i])));
-
-            if (img_out[i] > threshold)
-            {
-                double angle = std::atan2(gy.get()[i], gx.get()[i]);
-                Color c = colorGradient(angle);
-                img_out[i] = std::get<0>(c);
-                img_out[i + 1] = std::get<1>(c);
-                img_out[i + 2] = std::get<2>(c);
-            }
-            else
-            {
-                img_out[i + 1] = img_out[i];
-                img_out[i + 2] = img_out[i];
-            }
-        }
+        gradient(gy.get(), gx.get(), width, height, channels, img_out, threshold);
     }
     else
     {
         std::unique_ptr<float[]> tmpres = std::make_unique<float[]>(size);
-        for (size_t i = 0; i < size - channels + 1; i += channels)
-        {
-
-            //   img_out[i] = static_cast<uint8_t>(std::sqrt(std::pow((img_outX.get()[i]), 2) + std::sqrt(std::pow(img_outY.get()[i], 2))));
-            tmpres[i] = std::abs(gy.get()[i]) + std::abs(gx.get()[i]);
-            tmpres[i + 1] = img_out[i];
-            tmpres[i + 2] = img_out[i];
-        }
-
-        rescaleLightDark<T>(tmpres.get(), width, height, channels, img_out, threshold);
+        magnitude(gy.get(), gx.get(), width, height, channels, tmpres.get(), threshold);
+        rescaleLightDark(tmpres.get(), width, height, channels, img_out);
     }
 }
 
 template <typename T>
 void getGyGx(const T *img_in, size_t width, size_t height, size_t channels, float *img_outY, float *img_outX, size_t ksize, size_t gaussianKsize)
 {
-
     size_t size = width * height * channels;
 
     std::unique_ptr<T[]> img_ingray = std::make_unique<T[]>(size);
     std::unique_ptr<T[]> img_ingrayblur = std::make_unique<T[]>(size);
     gray<T>(img_in, width, height, channels, img_ingray.get(), 0.33f, 0.33f, 0.33f);
     gaussianBlurSeparate(img_ingray.get(), width, height, channels, img_ingrayblur.get(), gaussianKsize, EXTEND);
+
     std::unique_ptr<float[]> img_inY = std::make_unique<float[]>(size);
     std::unique_ptr<float[]> img_inX = std::make_unique<float[]>(size);
+
     auto kernelpair = sobelKernel(ksize);
     Kernel sy = kernelpair.first;
     Kernel sx = kernelpair.second;
@@ -97,6 +77,59 @@ void getGyGx(const T *img_in, size_t width, size_t height, size_t channels, floa
 
     convolution<float>(img_inY.get(), width, height, channels, img_outY, sy, EXTEND);
     convolution<float>(img_inX.get(), width, height, channels, img_outX, sx, EXTEND);
+}
+
+template <typename T>
+void magnitude(const float *gy, const float *gx, size_t width, size_t height, size_t channels, T *img_out, size_t threshold)
+{
+    size_t size = width * height * channels;
+    for (size_t i = 0; i < size - channels + 1; i += channels)
+    {
+        // img_out[i] = static_cast<uint8_t>(std::sqrt(std::pow((img_outX.get()[i]), 2) + std::sqrt(std::pow(img_outY.get()[i], 2))));
+        // abs(x) + abs(y) ≈ sqrt(x^2 + y), since abs is more efficent
+
+        img_out[i] = std::abs(gy[i]) + std::abs(gx[i]);
+        if (img_out[i] > static_cast<float>(threshold))
+        {
+            img_out[i + 1] = img_out[i];
+            img_out[i + 2] = img_out[i];
+        }
+        else
+        {
+            img_out[i] = 0;
+            img_out[i + 1] = img_out[i];
+            img_out[i + 2] = img_out[i];
+        }
+    }
+}
+
+template <typename T>
+void gradient(const float *gy, const float *gx, size_t width, size_t height, size_t channels, T *img_out, size_t threshold)
+{
+    size_t size = width * height * channels;
+    for (size_t i = 0; i < size - channels + 1; i += channels)
+    {
+
+        // img_out[i] = static_cast<uint8_t>(std::sqrt(std::pow((img_outX.get()[i]), 2) + std::sqrt(std::pow(img_outY.get()[i], 2))));
+        // abs(x) + abs(y) ≈ sqrt(x^2 + y), since abs is more efficent
+
+        img_out[i] = static_cast<T>((std::abs(gy[i]) + std::abs(gx[i])));
+
+        if (img_out[i] > threshold)
+        {
+            double angle = std::atan2(gy[i], gx[i]);
+            Color c = colorGradient(angle);
+            img_out[i] = std::get<0>(c);
+            img_out[i + 1] = std::get<1>(c);
+            img_out[i + 2] = std::get<2>(c);
+        }
+        else
+        {
+            img_out[i] = 0;
+            img_out[i + 1] = img_out[i];
+            img_out[i + 2] = img_out[i];
+        }
+    }
 }
 
 template <typename T>
@@ -181,34 +214,26 @@ Color colorGradient(double angle)
 }
 
 template <typename T>
-void rescaleLightDark(const float *img_in, size_t width, size_t height, size_t channels, T *img_out, uint8_t threshold)
+void rescaleLightDark(const float *img_in, size_t width, size_t height, size_t channels, T *img_out)
 {
-    float max = -1000;
+    int max = -100;
     size_t size = width * height * channels;
 
     for (size_t i = 0; i < size - channels + 1; i += channels)
-    {   
-        float tmp = static_cast<float>(img_in[i]);
+    {
+        int tmp = static_cast<int>(img_in[i]);
         if (tmp > max)
         {
             max = tmp;
         }
     }
 
-    float ratio = 255.f / max;
+    float ratio = 255.f / static_cast<float>(max);
 
     for (size_t i = 0; i < size - channels + 1; i += channels)
     {
-        img_out[i]  = static_cast<T>(img_in[i] * ratio);
-        if (img_out[i] > threshold)
-        {
-            img_out[i + 1] = img_out[i];
-            img_out[i + 2] = img_out[i];
-        }
-        else{
-            img_out[i] = 0;
-            img_out[i + 1] = 0;
-            img_out[i + 2] = 0;
-        }
+        img_out[i] = static_cast<T>(img_in[i] * ratio);
+        img_out[i + 1] = img_out[i];
+        img_out[i + 2] = img_out[i];
     }
 }
